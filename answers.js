b3a125fa7696760c784fe16c6e7529529024dad2
Augustin Delporte <delporteaugustin@gmail.com>
2016-04-15 17:39:57 +0000

    "Finished level 2"

--- answers.js
+++ answers.js
@@ -11,3 +11,19 @@ function printPositives (array) {
 
 var newArray = [2, 5, 6, -4, -8, -5, 6];
 printPositives(newArray);
+
+/*Exercice 2 - Similar to the previous exercise, write a function called getPositives 
+that takes an array and uses its filter method to return a new array with only the positive numbers*/
+var newArray2 = [2, 5, 6, 10, -8, -58, -4, -8, -5, 10, -11, 50];
+var newArray3 = [5, -15, 58, -100, 59, -800, 2, 0, -48, 55];
+
+function getPositives(num) {
+    if(num > 0) {
+        return num;
+    }
+}
+
+var result = newArray2.filter(getPositives);
+console.log(result);
+var result2 = newArray3.filter(getPositives);
+console.log(result2);
